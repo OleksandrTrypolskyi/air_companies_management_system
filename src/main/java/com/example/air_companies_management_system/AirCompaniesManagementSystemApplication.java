786928e4e3ab464a7fe35b7a483b4c6530cc47a7
TrypolskyiS <tripolskys@gmail.com>
2022-03-19 08:46:15 +0200

    Init commit

---
+++ src/main/java/com/example/air_companies_management_system/AirCompaniesManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.air_companies_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirCompaniesManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirCompaniesManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/air_companies_management_system/AirCompaniesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.air_companies_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirCompaniesManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
